fcd0e74eaa652f516b47cae1438f5e31da94db47
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-11-24 13:28:17 +0100

    add : aggiunto componenti per progetto

---
+++ JS/index.js
@@ -0,0 +1,8 @@
+// creazione dell alista della spesa in un array
+
+const araySpesa=["pomodori", "latte", "petto di pollo"] ;
+
+// prendere elemento lista della spesa da html
+
+const listaUl = document.getElementById("lista-spesa");
+
