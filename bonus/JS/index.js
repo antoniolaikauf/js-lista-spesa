5f880a2268831f47f4a8f388c0a66bf70f0d1cef
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-11-24 13:56:10 +0100

    add: aggiunto pulsante per aggiungere prodotti personali

--- bonus/JS/index.js
+++ bonus/JS/index.js
@@ -1,18 +1,32 @@
 // creazione dell alista della spesa in un array
 
-const araySpesa=["pomodori", "latte", "petto di pollo"] ;
-
+const arraySpesa=["pomodori", "latte", "petto di pollo"] ;
 // prendere elemento lista della spesa da html
 
 const listaUl = document.getElementById("lista-spesa");
 
+// prendere elemento da input 
+let itemsPersonale =document.getElementById("prodotto-personale");
+
+// aggiungere evento per mettere item scelto entro lista
+let button = document.getElementById("aggiungi");
+button.addEventListener("click", function () {
+    // item scelto dall'utente
+    let valore = itemsPersonale.value;
+    let itemUtente= document.createElement("li");
+    itemUtente.append(valore);
+    listaUl.append(itemUtente);
+    // svuotamento dell'input
+    itemsPersonale.value="";
+})
+
 // creazione del loop while
 let i=0;
-while (i < araySpesa.length) {
-    const itemsIesimo = araySpesa[i];
+while (i < arraySpesa.length) {
+    const itemsIesimo = arraySpesa[i];
     let item= document.createElement("li");
     item.append(itemsIesimo)
-    // farli leggere dentro html
+    // farli leggere dentro alla lista
     listaUl.append(item)
     // incremento
     i++
